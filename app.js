cfb283e162260992667a25a928d163ccdb728feb
llvovll89 <svvvs5579@naver.com>
2022-09-17 03:17:32 +0900

    update-v2

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup'),
+innerBox = document.querySelector('.inner_box');
 
 popup.addEventListener('click', () => {
     popup.classList.toggle('active');
+})
+
+innerBox.addEventListener('mouseover', ()=> {
+    innerBox.classList.add('active');
+})
+
+innerBox.addEventListener('mouseout', ()=> {
+    innerBox.classList.remove('active');
 })
